97c98e748383a4dd45e4b7003392f0cbd1be1c14
Dongmiansheng <dongmiansheng@parkingwang.com>
2018-10-25 09:53:30 +0800

    新增提示工具类例子

--- app/src/main/java/com/parkingwang/android/app/MainActivity.java
+++ app/src/main/java/com/parkingwang/android/app/MainActivity.java
@@ -3,8 +3,12 @@ package com.parkingwang.android.app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
 import com.parkingwang.android.SwDoubleClick;
+import com.parkingwang.android.view.SwLoading;
+import com.parkingwang.android.view.SwToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +27,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void success(final View view) {
+        loadingAfter(view, new Callback() {
+            @Override
+            public void function() {
+                SwToast.success(getApplicationContext()).show(((Button) view).getText().toString());
+            }
+        });
+    }
+
+    public void warn(final View view) {
+        loadingAfter(view, new Callback() {
+            @Override
+            public void function() {
+                SwToast.warning(getApplicationContext()).show(((Button) view).getText().toString());
+            }
+        });
+    }
+
+
+    public void error(final View view) {
+        loadingAfter(view, new Callback() {
+            @Override
+            public void function() {
+                SwToast.failed(getApplicationContext()).show(((Button) view).getText().toString());
+            }
+        });
+    }
+
+    private void loadingAfter(final View view, final Callback callback) {
+        final SwLoading loading = loading();
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loading.hide();
+                callback.function();
+            }
+        }, 1000);
+    }
+
+    private SwLoading loading() {
+        SwLoading loading = SwLoading.create(this);
+        loading.show();
+        return loading;
+    }
+
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return mDoubleClick.onKeyDown(keyCode, event) ||
                 super.onKeyDown(keyCode, event);
     }
+
+
+    private interface Callback {
+        void function();
+    }
+
 }

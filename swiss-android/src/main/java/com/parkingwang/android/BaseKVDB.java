fb196407e5415f265d63fb3e617693784132cd9f
DongMS <dongmiansheng@gmail.com>
2019-05-24 11:31:23 +0800

    移除SharePreferences异步线程池

--- swiss-android/src/main/java/com/parkingwang/android/BaseKVDB.java
+++ swiss-android/src/main/java/com/parkingwang/android/BaseKVDB.java
@@ -6,8 +6,6 @@ import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @author 陈小锅 (yoojia.chen@gmail.com)
@@ -17,85 +15,61 @@ class BaseKVDB {
 
     private final SharedPreferences mPreferences;
     private final Map<String, String> mMemCached = new HashMap<>();
-    private final ExecutorService mWriteThread;
     private final boolean mWriteAsync;
 
-    public BaseKVDB(String dbName, Context context){
+    public BaseKVDB(String dbName, Context context) {
         this(dbName, context, false);
     }
 
     public BaseKVDB(String dbName, Context context, boolean writeAsync) {
         mPreferences = context.getSharedPreferences(dbName, Context.MODE_PRIVATE);
         mWriteAsync = writeAsync;
-        if (writeAsync){
-            mWriteThread = Executors.newSingleThreadExecutor();
-        }else{
-            mWriteThread = null;
-        }
         reload();
     }
 
     @SuppressWarnings("unchecked")
-    public void reload(){
+    public void reload() {
         mMemCached.clear();
         mMemCached.putAll((Map<? extends String, ? extends String>) mPreferences.getAll());
     }
 
-    public String get(String key){
-        return get(key, null);
+    public String get(String key) {
+        return get(key, "");
     }
 
-    public String get(String key, String defValue){
-        if (mMemCached.containsKey(key)){
+    public String get(String key, String defValue) {
+        if (mMemCached.containsKey(key)) {
             return mMemCached.get(key);
-        }else{
+        } else {
             return defValue;
         }
     }
 
-    public void set(final String key, final String value){
+    private void commit(SharedPreferences.Editor editor) {
+        if (mWriteAsync) {
+            editor.apply();
+        } else {
+            editor.commit();
+        }
+    }
+
+    public void set(final String key, final String value) {
         if (value == null || value.isEmpty()) {
             Log.e("KVDB", "Ignore set empty value for KEY[" + key + "]. You can try remove(key).");
             return;
         }
         mMemCached.put(key, value);
-        final Runnable task = new Runnable() {
-            @Override public void run() {
-                mPreferences.edit().putString(key, value).apply();
-            }
-        };
-        if (mWriteAsync) {
-            mWriteThread.submit(task);
-        }else{
-            task.run();
-        }
+        SharedPreferences.Editor editor = mPreferences.edit().putString(key, value);
+        commit(editor);
     }
 
-    public void remove(final String key){
+    public void remove(final String key) {
         mMemCached.remove(key);
-        final Runnable task = new Runnable() {
-            @Override public void run() {
-                mPreferences.edit().remove(key).apply();
-            }
-        };
-        if (mWriteAsync) {
-            mWriteThread.submit(task);
-        }else{
-            task.run();
-        }
+        commit(mPreferences.edit().remove(key));
     }
 
-    public void clear(){
+    public void clear() {
         mMemCached.clear();
-        final Runnable task = new Runnable() {
-            @Override public void run() {
-                mPreferences.edit().clear().apply();
-            }
-        };
-        if (mWriteAsync) {
-            mWriteThread.submit(task);
-        }else{
-            task.run();
-        }
+        commit(mPreferences.edit().clear());
     }
 }

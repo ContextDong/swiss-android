a5adcdc9fcf432ce13c0adb5e337d5f4453747ec
DongMS <dongmiansheng@gmail.com>
2019-07-04 15:19:12 +0800

    修改图片沉浸式例子

--- app/src/main/java/com/parkingwang/android/app/bar/PicTransparentActivity.java
+++ app/src/main/java/com/parkingwang/android/app/bar/PicTransparentActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.parkingwang.android.SwBar;
 import com.parkingwang.android.app.R;
@@ -18,6 +19,8 @@ public class PicTransparentActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_transparent);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         SwBar.setStatusBarColor(this, Color.TRANSPARENT);
+        SwBar.addMarginTopEqualStatusBarHeight(toolbar);
     }
 }

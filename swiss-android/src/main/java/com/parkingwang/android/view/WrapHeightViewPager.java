f5f28d263805758cf2da37109eda630d50d8fb92
DongMS <dongmiansheng@gmail.com>
2019-09-12 13:59:46 +0800

    添加高度适配内容的ViewPager

---
+++ swiss-android/src/main/java/com/parkingwang/android/view/WrapHeightViewPager.java
@@ -0,0 +1,41 @@
+package com.parkingwang.android.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 高度适配内容的ViewPager.
+ *
+ * @author DongMS
+ * @since 2019/9/12
+ */
+public class WrapHeightViewPager extends ViewPager {
+
+    public WrapHeightViewPager(Context context) {
+        super(context);
+    }
+
+    public WrapHeightViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height) height = h;
+        }
+
+        height += getPaddingTop() + getPaddingBottom();
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}

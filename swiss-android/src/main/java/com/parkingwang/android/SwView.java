05d83e433c43b53de9fe392de0fd1b8d4be1b67c
DongMS <dongmiansheng@gmail.com>
2019-09-12 13:41:10 +0800

    添加扩大View的触摸面积方法

--- swiss-android/src/main/java/com/parkingwang/android/SwView.java
+++ swiss-android/src/main/java/com/parkingwang/android/SwView.java
@@ -1,10 +1,13 @@
 package com.parkingwang.android;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
+import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 /**
  * @author DongMS
@@ -49,6 +52,28 @@ public final class SwView {
 
     public static View inflate(final Context context, @LayoutRes final int layoutRes, final ViewGroup parentView, final boolean isAttachParentView) {
         return getLayoutInflater(context).inflate(layoutRes, parentView, isAttachParentView);
+    }
+
+    /**
+     * 扩大触摸面积
+     *
+     * @param additional 要增加的每个方向的面积
+     * @param view       要扩大触摸面积的View
+     */
+    public static void extendTouchRange(final int additional, final View view) {
+        view.post(() -> {
+            final Rect touchRect = new Rect();
+            view.getHitRect(touchRect);
+            touchRect.left -= additional;
+            touchRect.right += additional;
+            touchRect.top -= additional;
+            touchRect.bottom += additional;
+            TouchDelegate touchDelegate = new TouchDelegate(touchRect, view);
+            ViewParent parent = view.getParent();
+            if (parent instanceof View) {
+                ((View) parent).setTouchDelegate(touchDelegate);
+            }
+        });
     }
 
 }
